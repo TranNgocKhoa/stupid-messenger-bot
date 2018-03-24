8e82915b8f145be9eab75f78a8951e508b3b795e
15110064 <ngockhoatran1996@gmail.com>
2018-03-25 01:32:15 +0700

    commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=stupid-messenger-bot-Web-Deploy.pubxml -p:Password=Y3S86yZAkJmZbEhZsQq2d591ipHHpoafHFEWJriPDeK1SH7H0LRtdl4d9yPj
+
